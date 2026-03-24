ae75df03bb4cb08d92353d7e21f2ffe58b3f8123
Saksham Verma <sakshamverma2205@gmail.com>
2026-03-24 08:51:45 +0530

    Use Case 1

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,14 @@
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("   Welcome to Book My Stay Application ");
+        System.out.println("=======================================");
+
+        System.out.println("Application Name : Hotel Booking System");
+        System.out.println("Version          : v1.0");
+
+        System.out.println("Application started successfully!");
+    }
+}
